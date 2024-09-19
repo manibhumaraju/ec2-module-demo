ba263e2b9e91bf8507418e6efa23ea93540a3ce8
manibhumaraju <manishvarmab@gmail.com>
2024-09-19 19:51:10 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ module "ec2" {
     # these are variables expected module, not arguments of resource definition
     # you can also create variables or else you can directly use values here
     ami_id = var.ami_something
-    instance_type = "t3.large"
-    vpc_security_group_ids = ["sg-04171accc0f35de44"]
+    instance_type = "t3.micro"
+    security_group_ids = ["sg-04171accc0f35de44"]
 }
